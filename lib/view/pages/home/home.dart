8b2983b9b7bd3079e685c7d4d3eb5effd35224fc
saif <saifr383@gmail.com>
2022-03-27 04:59:25 +0500

    firebase

--- lib/view/pages/home/home.dart
+++ lib/view/pages/home/home.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tech_event_registration/utils/const/colors.dart';
 import 'package:tech_event_registration/view/pages/root/event_detail_screen.dart';
 import 'package:tech_event_registration/view/pages/root/widgets/eventlist.dart';
 import 'package:tech_event_registration/view/pages/root/widgets/feature.dart';
+
+import '../../../controllers/auth_controller.dart';
+import '../../../controllers/event.dart';
+import '../root/widgets/drwaer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -20,13 +25,6 @@ class _HomePageState extends State<HomePage> {
     AuthController controller = Get.find();
     if(controller.userModel == null) {
       await controller.getUser(FirebaseAuth.instance.currentUser!.uid);
-    }
-    if(controller.userModel == null) {
-      log("still null");
-    }
-    else {
-      log("not null");
-      log(controller.userModel!.name!);
     }
 
 
@@ -46,49 +44,51 @@ class _HomePageState extends State<HomePage> {
               future: Future.delayed(Duration(seconds: 6)),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting)
-                  {
-                    return Center(child: CircularProgressIndicator(),);
-                  }
-                else
-                  {
-                    return Container(
-                      height: Get.height,width: Get.width,
-                      child: Stack(overflow:Overflow.visible,
-                        children: [
-                          Positioned(child: Container(
-
-                    child: Column(
+                {
+                  return Center(child: CircularProgressIndicator(),);
+                }
+                else {return Container
+                  (
+                    height: Get.height,width: Get.width,
+                    child: Stack(overflow:Overflow.visible,
                       children: [
-                        Image.asset('assets/images/logo.png',color: Colors.white,height: 80,width: 200,)
-,
-                        Feature(),
+                        Positioned(child: Container(
+
+                          child: Column(
+                            children: [
+                              Image.asset('assets/images/logo.png',color: Colors.white,height: 80,width: 200,)
+                              ,
+                              Feature(),
+
+                            ],
+                          ),
+                          height: Get.height/2.1,
+                          width: Get.width,
+
+                          decoration: const BoxDecoration(gradient: LinearGradient(
+                            colors: [ColorResources.COLOR_PRIMARY, Color(0x9C6127FF)],
+                            begin: Alignment.bottomRight,end: Alignment.topLeft,
+                            stops: [0.1, 0.6],
+                          ),
+
+                          ),
+                        ),top: 0,left: 0,),
+                        Positioned(child:  Container(decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(40),
+                                topRight: Radius.circular(40))),
+                            width: Get.width,child: Events()
+                        ),top:Get.height/2.35 ,left: 0,),
+                        EventDetailScreen()
 
                       ],
-                    ),
-                    height: Get.height/2.1,
-                    width: Get.width,
-
-                    decoration: const BoxDecoration(gradient: LinearGradient(
-                      colors: [ColorResources.COLOR_PRIMARY, Color(0x9C6127FF)],
-                      begin: Alignment.bottomRight,end: Alignment.topLeft,
-                      stops: [0.1, 0.6],
-                    ),
-
-                    ),
-                  ),top: 0,left: 0,),
-                  Positioned(child:  Container(decoration: BoxDecoration(
-                    color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(40),
-                      topRight: Radius.circular(40))),
-                      width: Get.width,child: Events()
-                  ),top:Get.height/2.35 ,left: 0,),
-                  EventDetailScreen()
-
-                ],
-              ),
-            ),
+                    ))
+                ;}
+              }
+                )
           );
+
         }
       ),
     );
